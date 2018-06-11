2e5d346440caa66ee72385fc6ffbe5c7eec344e1
Amit Harlev <amit.harlev@gmail.com>
2018-06-11 15:12:40 -0700

    Added Material-UI and nice button to login page

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,39 @@
 import React, { Component, ReactFragment } from 'react';
 import logo from './logo.svg';
 import fire from './fire';
+import Button from '@material-ui/core/Button';
 
 class Login extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {};
+
+        let provider = new fire.auth.GoogleAuthProvider();
+    }
+
+    // handleGoogleLogin() {
+    //     loginWithGoogle()
+    //         .catch(function (error) {
+    //             alert(error); // or show toast
+    //             localStorage.removeItem(firebaseAuthKey);
+    //         });
+    //     localStorage.setItem(firebaseAuthKey, "1");
+    // }
+
 	render() {
 		return  (
-					<h1>Login Here</h1>
+                    <div>
+                        <h1>Login</h1>
+                        <div>
+                        <Button variant="contained" color='secondary'>
+                            Sign in with Google
+                        </Button>
+                        </div>
+                    </div>
 		        );
-	}
+    }
+
 }
 
 export default Login;
