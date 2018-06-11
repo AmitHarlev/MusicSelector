e48fd91ae97262a7a7edc2565bc3b2fb98f53a54
Amit Harlev <amit.harlev@gmail.com>
2018-06-11 10:27:27 -0700

    Got through weird github problems

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import fire from './fire';
 
+var database = fire.database();
+
 class App extends Component {
+
   render() {
     return (
       <div>
@@ -17,9 +20,16 @@ class MainPanel extends Component {
   constructor(props){
     super(props);
     this.state = {
-      items: ["Call Me Maybe"],
+      items: [],
       textBoxValue: ""
     }
+    fire.database().ref('songs').once('value').then((snapshot) => {
+      const items = Object.keys(snapshot.val()).map(val => snapshot.val()[val])
+      console.log(items);
+      this.setState({
+        items: items
+      });
+    });
   }
 
   handleChange = (e) => {
@@ -28,16 +38,26 @@ class MainPanel extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault()
+
+    database.ref('songs').push({name:this.state.textBoxValue,value:0});
+
       this.setState({
-        items: this.state.items.concat(this.state.textBoxValue),
+        items: this.state.items.concat({name:this.state.textBoxValue,value:0}),
         textBoxValue: ""
       });
   }
 
   SongList = () => {
     return this.state.items.map((item, index) => (
-        <li key={index}><span>{item} <UpDoot/></span></li>
+      <span><li key={item.name}>{item.name} <UpDoot name={item}/></li></span>
     ));
+  }
+
+  swapArrayElements = (array,a,b) => {
+    const temp = array[a];
+    array[a] = array[b]
+    array[b] = temp
+    return array;
   }
 
 
@@ -47,7 +67,9 @@ class MainPanel extends Component {
         <form onSubmit={this.handleSubmit}>
           <input type="text" value={this.state.textBoxValue} onChange={this.handleChange}></input>
         </form>
-        <this.SongList/>
+        <ul>
+          <this.SongList/>
+        </ul>
       </div>
     )}
 }
@@ -57,22 +79,26 @@ class UpDoot extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      vote: 0
+      vote: props.name.value,
+      clicked: false
     };
 
   }
 
-  incrementVote = () => {
+  handleClick = () => {
     this.setState({
-      vote: this.state.vote + 1
+      vote: this.state.clicked ? this.state.vote - 1 : this.state.vote + 1,
+      clicked: !this.state.clicked
     });
   }
 
   render() {
-    return (<div>
-      <button onClick={this.incrementVote}>Click Me</button>
-      <span>{this.state.vote}</span>
-    </div>);
+    return (
+    <span>
+      <button onClick={this.handleClick}>Click Me</button>
+      ---{this.state.vote}
+    </span>
+      );
   }
 }
 
