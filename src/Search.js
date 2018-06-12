3d08a89477eabb948187ef87c7253212343145ba
Amit Harlev <amit.harlev@gmail.com>
2018-06-12 09:22:42 -0700

    Added Search Page for Ryan

---
+++ src/Search.js
@@ -0,0 +1,22 @@
+import fire from './fire';
+import React, { Component } from 'react';
+import SubmitSong from './SubmitSong';
+
+class Search extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: []
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<SubmitSong/>
+			</div>
+		)
+	}
+}
+
+export default Search
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Login from './Login'
+import Search from './Search'
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ ReactDOM.render(
 			<Switch>
 				<Route path="/login" component={Login}/>
 				<Route path="/app" component={App}/>
+				<Route path="/search" component={Search}/>
 				<Redirect from="/" to="login" />
 			</Switch>
 		</Router>
