d0d12d39f8c728c05cc9c974781ad61f47fc8707
BertTurtle <mbentley124@gmail.com>
2018-06-12 09:25:35 -0700

    Fixed merge errors

--- src/Login.js
+++ src/Login.js
@@ -7,65 +7,6 @@ import SignOutButton from './SignOutButton'
 
 class Login extends Component {
 
-<<<<<<< HEAD
-    constructor(props) {
-        super(props);
-        this.state = {};
-
-        this.provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().useDeviceLanguage();
-
-        firebase.auth().onAuthStateChanged(function(user) {
-            if (user) {
-              // User is signed in.
-            //   var displayName = user.displayName;
-            //   var email = user.email;
-            //   var emailVerified = user.emailVerified;
-            //   var photoURL = user.photoURL;
-            //   var isAnonymous = user.isAnonymous;
-            //   var uid = user.uid;
-            //   var providerData = user.providerData;
-              props.history.push("/app");
-              // ...
-            } else {
-                props.history.push("/login");
-              // ...
-            }
-          });
-
-          firebase.auth().getRedirectResult().then(function(result) {
-            if (result.credential) {
-              // This gives you a Google Access Token. You can use it to access the Google API.
-              var token = result.credential.accessToken;
-              // ...
-            }
-            // The signed-in user info.
-            var user = result.user;
-          }).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
-          });
-
-    }
-
-    handleGoogleLogin = () => {
-        firebase.auth().signInWithRedirect(this.provider);
-    }
-
-    handleSignOut = () => {
-        firebase.auth().signOut().then(function() {
-            // Sign-out successful.
-          }).catch(function(error) {
-            // An error happened.
-          });
-    }
-=======
 	constructor(props) {
 		super(props);
 		this.state = {};
@@ -90,6 +31,26 @@ class Login extends Component {
 				// ...
 			}
 		});
+
+		firebase.auth().getRedirectResult().then(function (result) {
+			if (result.credential) {
+				// This gives you a Google Access Token. You can use it to access the Google API.
+				var token = result.credential.accessToken;
+				// ...
+			}
+			// The signed-in user info.
+			var user = result.user;
+		}).catch(function (error) {
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			// The email of the user's account used.
+			var email = error.email;
+			// The firebase.auth.AuthCredential type that was used.
+			var credential = error.credential;
+			// ...
+		});
+
 	}
 
 	handleGoogleLogin = () => {
@@ -103,7 +64,6 @@ class Login extends Component {
 			// An error happened.
 		});
 	}
->>>>>>> 0224ec4fa00662cfcd9f531fd8f2bba3e10081f0
 
 	render() {
 		return (
