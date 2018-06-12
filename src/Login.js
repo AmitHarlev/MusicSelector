0224ec4fa00662cfcd9f531fd8f2bba3e10081f0
BertTurtle <mbentley124@gmail.com>
2018-06-12 09:19:35 -0700

    Fixed indentation

--- src/Login.js
+++ src/Login.js
@@ -7,57 +7,56 @@ import SignOutButton from './SignOutButton'
 
 class Login extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {};
+	constructor(props) {
+		super(props);
+		this.state = {};
 
-        this.provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().useDeviceLanguage();
+		this.provider = new firebase.auth.GoogleAuthProvider();
+		firebase.auth().useDeviceLanguage();
 
-        firebase.auth().onAuthStateChanged(function(user) {
-            if (user) {
-              // User is signed in.
-            //   var displayName = user.displayName;
-            //   var email = user.email;
-            //   var emailVerified = user.emailVerified;
-            //   var photoURL = user.photoURL;
-            //   var isAnonymous = user.isAnonymous;
-            //   var uid = user.uid;
-            //   var providerData = user.providerData;
-              props.history.push("/app");
-              // ...
-            } else {
-                props.history.push("/login");
-              // ...
-            }
-          });
-    }
+		firebase.auth().onAuthStateChanged(function (user) {
+			if (user) {
+				// User is signed in.
+				//   var displayName = user.displayName;
+				//   var email = user.email;
+				//   var emailVerified = user.emailVerified;
+				//   var photoURL = user.photoURL;
+				//   var isAnonymous = user.isAnonymous;
+				//   var uid = user.uid;
+				//   var providerData = user.providerData;
+				props.history.push("/app");
+				// ...
+			} else {
+				props.history.push("/login");
+				// ...
+			}
+		});
+	}
 
-    handleGoogleLogin = () => {
-        firebase.auth().signInWithRedirect(this.provider);
-    }
+	handleGoogleLogin = () => {
+		firebase.auth().signInWithRedirect(this.provider);
+	}
 
-    handleSignOut = () => {
-        firebase.auth().signOut().then(function() {
-            // Sign-out successful.
-          }).catch(function(error) {
-            // An error happened.
-          });
-    }
+	handleSignOut = () => {
+		firebase.auth().signOut().then(function () {
+			// Sign-out successful.
+		}).catch(function (error) {
+			// An error happened.
+		});
+	}
 
 	render() {
-		return  (
-                    <div>
-                        <h1>Login</h1>
-                        <div>
-                        <Button variant="contained" color='secondary' onClick={this.handleGoogleLogin}>
-                            Sign in with Google
-                        </Button>
-                        <SignOutButton/>
-                        </div>
-                    </div>
-		        );
-    }
+		return (
+			<div>
+				<h1>Login</h1>
+				<div>
+					<Button variant="contained" color='secondary' onClick={this.handleGoogleLogin}>
+						Sign in with Google</Button>
+					<SignOutButton />
+				</div>
+			</div>
+		);
+	}
 
 }
 
