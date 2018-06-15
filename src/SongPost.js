d0504bc1b5e5922587db2a5beba85e3fef4b1a1d
Amit Harlev <amit.harlev@gmail.com>
2018-06-15 12:01:14 -0700

    added admin checking and submission (incomplete)

--- src/SongPost.js
+++ src/SongPost.js
@@ -18,6 +18,7 @@ class SongPost extends React.Component {
 						<UpDoot uid={this.props.uid} items={this.props.items} id={this.props.id} login={this.props.login} />
 						<img src={this.props.items[this.props.id].thumbnail} alt="Song Thumbnail" align="middle" style={{marginLeft:"20px", marginRight:"20px"}}/>
 						<a href={this.props.items[this.props.id].link} style={{textDecoration:"none"}}>{this.props.items[this.props.id].name}</a>
+						{this.props.admin ?`    Submitted by ${this.props.userName}` : ""}
 					</li>
 				</Paper>
 			</span>
