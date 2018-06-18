09901c25ea4f37ccca076e102eb9529555f3df8d
Raian <rycen618@gmail.com>
2018-06-18 09:08:44 -0700

    made updoot disabled looking before login

--- src/SongPost.js
+++ src/SongPost.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import UpDoot from './UpDoot';
-import { Paper, Typography } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
 import 'typeface-roboto'
 
 class SongPost extends React.Component {
@@ -16,11 +16,9 @@ class SongPost extends React.Component {
 			<span>
 				<Paper elevation = {4} style = {{margin:"20px"}}>
 					<li style={style}>
-						<Typography>
-							<UpDoot uid={this.props.uid} items={this.props.items} id={this.props.id} login={this.props.login} />
-							<img src={this.props.items[this.props.id].thumbnail} alt="Song Thumbnail" align="middle" style={{marginLeft:"20px", marginRight:"20px"}}/>
-							<a href={this.props.items[this.props.id].link} style={{textDecoration:"none"}}>{this.props.items[this.props.id].name}</a>
-						</Typography>
+						<UpDoot uid={this.props.uid} items={this.props.items} id={this.props.id} login={this.props.login} align="middle"/>
+						<img src={this.props.items[this.props.id].thumbnail} alt="Song Thumbnail" align="middle" style={{marginLeft:"20px", marginRight	:"20px"}}/>
+						<a href={this.props.items[this.props.id].link} style={{textDecoration:"none", fontFamily:"roboto"}}>{this.props.items[this.props.id].name}</a>
 					</li>
 				</Paper>
 			</span>
